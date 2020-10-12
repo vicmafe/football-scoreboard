207f07dfcba9325d9c7cfb2865a33cbea644fffb
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-10-12 20:58:09 -0300

    create compoment counter with two buttons

--- football-scoreboard/src/App.js
+++ football-scoreboard/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Counter from './Component/Counter.js';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      <Counter />
     </div>
   );
 }
---
+++ football-scoreboard/src/Component/Counter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class counter extends Component {
+  render() {
+    return (
+      <div>
+        <button>+</button>
+        <button>-</button>
+      </div>
+    );
+  }
+}
+ 
+export default counter;
