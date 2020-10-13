8b7804a05a88a5e7c2ba3fdc9557a66754fe48c8
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-10-12 21:14:36 -0300

    add score in counter state

--- football-scoreboard/src/Component/Counter.js
+++ football-scoreboard/src/Component/Counter.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 
 class counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      score: 0,
+    }
+  }
+
   render() {
     return (
       <div>
+        <div>{this.state.score}</div>
         <button>+</button>
         <button>-</button>
       </div>
