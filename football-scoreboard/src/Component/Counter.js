8fb0f5d7ca59452dc81e484f9ac11d6179e7fbcb
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-10-12 21:46:29 -0300

    functions for click event changing counter state

--- football-scoreboard/src/Component/Counter.js
+++ football-scoreboard/src/Component/Counter.js
@@ -6,14 +6,28 @@ class counter extends Component {
     this.state = {
       score: 0,
     }
+    this.handleClickPlus = this.handleClickPlus.bind(this);
+    this.handleClickLess = this.handleClickLess.bind(this);
+  }
+
+  handleClickPlus() {
+    this.setState({
+      score: this.state.score + 1
+    });
+  }
+
+  handleClickLess() {
+    this.setState({
+      score: this.state.score - 1
+    });
   }
 
   render() {
     return (
       <div>
         <div>{this.state.score}</div>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={this.handleClickPlus}>+</button>
+        <button onClick={this.handleClickLess}>-</button>
       </div>
     );
   }
