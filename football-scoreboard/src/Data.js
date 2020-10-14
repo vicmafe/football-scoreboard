fee1cdd2e4ca4ecfcdabfa209c1b71d667e42442
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2020-10-14 16:01:01 -0300

    register data.js

---
+++ football-scoreboard/src/Data.js
@@ -0,0 +1,19 @@
+const matchData = {
+  estadio: 'Olimpico Monumental',
+  cidade: 'Porto Alegre - RS',
+  data: 00/00/0000,
+  horario: 16
+}
+
+const round = {
+  turno: 4,
+  rodada: 6
+}
+
+const homeTeam = {
+  nome: 'Grêmio'
+}
+
+const visitorTeam = {
+  nome: 'Avaí'
+}
